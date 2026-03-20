b8f7a52890161d4e5512f454d608ff1d2b5cf5ac
Samuel Pontefract <zia32kvs@duck.com>
2026-03-20 23:12:59 +0100

    Improved slider dragging
    - Much smoother and easier to control now

--- app/src/main/java/com/turbofan3360/openeq/ui/components/VerticalSlider.kt
+++ app/src/main/java/com/turbofan3360/openeq/ui/components/VerticalSlider.kt
@@ -1,9 +1,7 @@
 package com.turbofan3360.openeq.ui.components
 
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.draggable
-import androidx.compose.foundation.gestures.rememberDraggableState
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.height
 import androidx.compose.ui.unit.Dp
@@ -14,6 +12,11 @@ import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.layout.positionOnScreen
 import androidx.compose.ui.platform.LocalDensity
@@ -34,30 +37,35 @@ fun VerticalSlider(
 
     // Calculates the position offsets for the thumb circle
     val heightPx = with(LocalDensity.current) {height.toPx()}
-    val thumbCircleOffsets = Offset(x=heightToWidth*heightPx,
+    val circleOffsets = Offset(x=heightToWidth*heightPx,
         y=(heightPx/(valueRange.endInclusive - valueRange.start))*(valueRange.endInclusive - value)
     )
+    var canvasPos by remember{ mutableStateOf(Offset.Zero) }
+    var thumbPos by remember{ mutableStateOf(Offset.Zero) }
 
     // Creates the canvas
     Canvas(
         modifier = Modifier
             .height(height)
             .width(height*heightToWidth*2f)
-            // Making sliders draggable
-            .draggable(
-                // Which orientation to drag them in
-                orientation = Orientation.Vertical,
-                // What to do when dragged
-                state = rememberDraggableState { dragChange ->
-                    // TODO: Make the sensitivity change value here adaptive
-                    onValueChange((value-0.04f*dragChange).coerceIn(valueRange.start, valueRange.endInclusive))
-                }
-            )
             // Hands back coordinates of thumb circle on slider
             .onGloballyPositioned { coordinates ->
-                val canvasPos = coordinates.positionOnScreen()
+                canvasPos = coordinates.positionOnScreen()
 
-                updateThumbPosition(canvasPos + thumbCircleOffsets)
+                updateThumbPosition(canvasPos + circleOffsets)
+            }
+            // Making sliders draggable
+            .pointerInput(Unit) {
+                detectDragGestures(
+                    onDragStart = { startPos: Offset -> thumbPos = startPos },
+                    onDragEnd = { thumbPos = Offset.Zero },
+                    onDragCancel = { thumbPos = Offset.Zero },
+                    onDrag = { _, dragDelta ->
+                        thumbPos += dragDelta
+                        // Computing the slider value from the pixel positions
+                        onValueChange((valueRange.endInclusive - (thumbPos.y)/(heightPx)*(valueRange.endInclusive - valueRange.start)).coerceIn(valueRange.start, valueRange.endInclusive))
+                    }
+                )
             }
     ) {
         // Draws the central bar of the slider (with rounded ends)
@@ -65,17 +73,17 @@ fun VerticalSlider(
             color = trackColor,
             style = Fill,
             alpha = 0.75f,
-            size = Size(0.5f*heightToWidth*height.toPx(), height.toPx()),
-            cornerRadius = CornerRadius(2f*heightToWidth*height.toPx()),
-            topLeft = Offset(x=0.75f*heightToWidth*height.toPx(), y=0f)
+            size = Size(0.5f*heightToWidth*heightPx, heightPx),
+            cornerRadius = CornerRadius(2f*heightToWidth*heightPx),
+            topLeft = Offset(x=0.75f*heightToWidth*heightPx, y=0f)
         )
         // Draws the circle
         drawCircle(
             color = thumbColor,
             style = Fill,
-            radius = heightToWidth*height.toPx(),
+            radius = heightToWidth*heightPx,
             // Calculating where the center should be based on the range and value of the slider
-            center = thumbCircleOffsets
+            center = circleOffsets
         )
     }
 }
